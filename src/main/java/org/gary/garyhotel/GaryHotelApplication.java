5e83308b6131ba869b4e8e28e03fc6beb51bd3c2
LudekFiser <fiserlu1@uhk.cz>
2025-02-20 09:43:15 +0100

    Creating backend

---
+++ src/main/java/org/gary/garyhotel/GaryHotelApplication.java
@@ -0,0 +1,13 @@
+package org.gary.garyhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaryHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaryHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gary/garyhotel/GaryHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gary.garyhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaryHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
